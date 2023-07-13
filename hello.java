dfa3162093c58f4217c40349beb3f62cc65d4dec
Yusuf Ayyub <yayyub85@gmail.com>
2023-07-13 21:20:09 +0100

    variables in java

--- hello.java
+++ hello.java
@@ -1,5 +1,55 @@
 class hello {
     public static void main(String a[]) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println(3 + 5);
+        int num1 = 3;
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println(result);
+
+        int num3 = 9;
+        System.out.println(num3);
+
+        byte by = 127;
+        System.out.println(by);
+
+        short sh = 558;
+        System.out.println(sh);
+
+        long l = 5854l;
+        System.out.println(l);
+
+        float f = 5.8f;
+        System.out.println(f);
+
+        double d = 5.8;
+        System.out.println(d);
+
+        char c = 'b';
+        System.out.println(c);
+
+        boolean b = true;
+        System.out.println(b);
+
+        // Literal Values
+
+        int num4 = 0b101; // Binary - 5
+        System.out.println(num4);
+
+        int num5 = 0x7E; // hex - 126
+        System.out.println(num5);
+
+        int num6 = 10_00_00_000; // underscores for easy counting of digits
+        System.out.println(num6);
+
+        double num7 = 56; // double digits
+        System.out.println(num7);
+
+        double num8 = 12e10; // double digits
+        System.out.println(num8);
+
+        c++; // adds to the variable in increments,
+        System.out.println(c);
     }
 }

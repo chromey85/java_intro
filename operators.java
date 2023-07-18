49a00ac5e425ddaabda676edc6227cbff48c2585
Yusuf Ayyub <yayyub85@gmail.com>
2023-07-18 22:42:17 +0100

    introduction to operators

--- operators.java
+++ operators.java
@@ -1,5 +1,44 @@
 public class operators {
     public static void main(String args[]) {
+        int num1 = 7;
+        int num2 = 5;
+
+        // Standard operators
+
+        // int resultAdd = num1 + num2;
+        // System.out.println(resultAdd);
+
+        // int resultSubtract = num1 - num2;
+        // System.out.println(resultSubtract);
+
+        // int resultMultiply = num1 * num2;
+        // System.out.println(resultMultiply);
+
+        // int resultDivide = num1 % num2; // when using the / operator for division, it
+        // is best to use % Modulo as / only
+        // // outputs whole numbers whereas % outputs the remainders
+        // System.out.println(resultDivide);
+
+        // num1 = num1 + 2; // this increases the value and assigns the new value to the
+        // variable.
+        // System.out.println(num1);
+
+        // num1 += 2;
+        // System.out.println(num1);
+
+        // num1++;
+        // System.out.println(num1);
+
+        // num1--; // post increment or --num1 is post increment
+        // System.out.println(num1);
+
+        // int result = ++num2; // This will first increment THEN add it to the value of
+        // num2,
+        // System.out.println(result);
+
+        // int result1 = num2++; // This will first fetch the value of num2, THEN it
+        // will increment it
+        // System.out.println(result1);
 
     }
 }

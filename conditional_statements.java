078b83dd0a5eb6785678f615f82f867de07ec3a4
Yusuf Ayyub <yayyub85@gmail.com>
2023-07-21 00:09:10 +0100

    intro to conditional statements, switch statements

--- conditional_statements.java
+++ conditional_statements.java
@@ -1,7 +1,9 @@
 public class conditional_statements {
     public static void main(String[] args) {
 
-        int x = 0;
+        // // ----------------if statements
+
+        int x = 8;
         String result = "";
 
         // if (x > 10) {
@@ -22,7 +24,40 @@ public class conditional_statements {
 
         // System.out.println(result);
 
-        result = x % 2 == 0 ? "Its Even" : "Its Odd";
-        System.out.println(result);
+        // // --------------------- Ternary operator
+
+        // result = x % 2 == 0 ? "Its Even" : "Its Odd";
+        // System.out.println(result);
+
+        // // ------------------ switch Statements
+
+        switch (x) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("There are only 7 days in a week so pick a number between 1 and 7");
+                break;
+        }
+
     }
 }

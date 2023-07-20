17f43059b590016045596c280a057846d68a6837
Yusuf Ayyub <yayyub85@gmail.com>
2023-07-20 23:46:59 +0100

    intro to conditional statements, if statements

--- conditional_statements.java
+++ conditional_statements.java
@@ -1,18 +1,25 @@
 public class conditional_statements {
     public static void main(String[] args) {
 
-        int x = 18;
+        int x = 0;
+        String result = "";
 
-        if (x > 10) {
+        // if (x > 10) {
 
-            System.out.println("Thats right");
-            System.out.println("its bigger");
+        // System.out.println("Thats right");
+        // System.out.println("its bigger");
 
-        }
+        // }
 
+        // else
+
+        // System.out.println("Thats wrong");
+
+        if (x % 2 == 0)
+            result = "It's Even";
         else
+            result = "It's Odd";
 
-            System.out.println("Thats wrong");
-
+        System.out.println(result);
     }
 }

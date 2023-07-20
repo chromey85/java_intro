154c0bd965b7eb6488e8fb1ac90c5d66ae4d43d8
Yusuf Ayyub <yayyub85@gmail.com>
2023-07-20 23:18:48 +0100

    intro to conditional statements

---
+++ conditional_statements.java
@@ -0,0 +1,17 @@
+public class conditional_statements {
+    public static void main(String[] args) {
+
+        int x = 18;
+
+        if (x > 10) {
+
+            System.out.println("Thats right");
+
+        } else {
+
+            System.out.println("Thats wrong");
+
+        }
+
+    }
+}

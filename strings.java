b96aca6867014ac943533e7c13ff06e817ece74c
Yusuf Ayyub <yayyub85@gmail.com>
2023-07-27 18:01:12 +0100

    refresher on Strings

---
+++ strings.java
@@ -0,0 +1,12 @@
+public class strings {
+    public static void main(String[] args) {
+        // String name = new String("Yusuf"); // This is too extra
+        String name = "Yusuf"; // This is standard practice
+
+        System.out.println("Hello " + name);
+        System.out.println(name.hashCode()); // Shows the HashCode of the variable
+        System.out.println(name.charAt(1));// Shows the character at the position in the brackets of the variable
+        System.out.println(name.concat(" Ayyub")); // add to the end of the variable
+
+    }
+}

2c242c4d8e64d7819e1ba6b50a601be5c5dd2170
Yusuf Ayyub <yayyub85@gmail.com>
2023-08-07 12:19:25 +0100

    explenantion of this. of the object

--- encapsulation.java
+++ encapsulation.java
@@ -6,16 +6,16 @@ class Human {
         return age;
     }
 
-    public void setAge(int a) {
-        age = a;
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String n) {
-        name = n;
+    public void setName(String name) {
+        this.name = name;
     }
 
 }

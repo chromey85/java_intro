f1bdfbcd577ab13b06c444122daf807791820bf5
Yusuf Ayyub <yayyub85@gmail.com>
2023-09-19 21:38:14 +0100

    introduced to Functional interface and Lambda expressions (like arrow functions)

---
+++ functional_interface/Functional_interface.java
@@ -0,0 +1,41 @@
+
+@FunctionalInterface // FunctionalInterface only has 1 method in it
+// interface A {
+// void show(int i);
+// }
+
+// class B implements A {
+// public void show() {
+// System.out.println("in Show");
+// }
+// }
+
+interface A {
+    int add(int i, int j);
+}
+
+public class Functional_interface {
+    public static void main(String[] args) {
+
+        // A obj = new A() {
+
+        // public void show(int i) {
+        // System.out.println("in Show " + i);
+        // }
+
+        // };
+
+        // this is called a Lambda Function, it replaces the 'new A', like arrow
+        // function in js.
+
+        // A obj = (i) -> System.out.println("in Show " + i);
+
+        // obj.show(5);
+
+        A obj = (int i, int j) -> return i + j;
+
+        int result = obj.add(5, 4);
+        System.out.println(result);
+
+    }
+}

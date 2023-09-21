11021990f55e7e6dbe6609972743ab99c368f496
Yusuf Ayyub <yayyub85@gmail.com>
2023-09-21 18:10:58 +0100

    introduction to integer input

---
+++ User_input/Inputs.java
@@ -0,0 +1,30 @@
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.Scanner;
+
+public class Inputs {
+
+    public static void main(String[] args) {
+        // System.out.println("Enter a number");
+
+        // ----- this was an old way of accepting number input -----//
+        // int num = System.in.read(); // this will return an ascii value
+
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // BufferedReader bf = new BufferedReader(in);
+
+        // System.out.println(num - 48);
+
+        // int num = Integer.parseInt(bf.readLine());
+
+        // bf.close();
+
+        // Scanner sc = new Scanner(System.in);
+
+        // int num = sc.nextInt();
+
+        // System.out.println(num);
+
+    }
+}
